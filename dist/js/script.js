fd360168e1d94aa503d81dc37c900f3d3d953074
GotaKusuma <gotama@undiksha.ac.id>
2022-09-19 16:12:27 +0800

    update cing

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,13 +27,45 @@ window.addEventListener('click', function (e) {
 });
 
 // darkmode
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
 
-darkToggle.addEventListener('click', function () {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-    } else {
-        html.classList.remove('dark');
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const iconToggle = () => {
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none");
+};
+
+
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+        moonIcon.classList.add("display-none");
+        return;
     }
+    sunIcon.classList.add("display-none");
+};
+
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+};
+
+sunIcon.addEventListener("click", () => {
+    themeSwitch();
 });
+
+moonIcon.addEventListener("click", () => {
+    themeSwitch();
+});
+
+themeCheck();
